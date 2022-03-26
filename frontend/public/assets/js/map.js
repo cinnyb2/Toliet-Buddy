59f311b504420b3694191f4ac58b644f3f9b617c
benthecoder <benthecoder07@gmail.com>
2022-03-26 17:31:53 -0500

    switch from inline loading to dynamic loading

--- frontend/public/assets/js/map.js
+++ frontend/public/assets/js/map.js
@@ -1,26 +1,29 @@
 // Initialize and add the map
 
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+  apiKey: process.env.GC_API_KEY,
+  version: 'weekly',
+  ...additionalOptions,
+});
 
 let map;
 const SAN_FRANCISCO_BOUNDS = {
   north: 37.84,
   south: 37.71,
   west: -122.52,
-  east: -122.35
+  east: -122.35,
 };
-const SAN_FRANCISCO = { lat: 37.76, lng: -122.45};
+const SAN_FRANCISCO = { lat: 37.76, lng: -122.45 };
 
-var myStyles =[
+var myStyles = [
   {
-      featureType: "poi",
-      elementType: "labels",
-      stylers: [
-            { visibility: "off" }
-      ]
-  }
+    featureType: 'poi',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }],
+  },
 ];
-
-
 
 function addMarker(lat, lng, title) {
   // var myLatlng = new google.maps.LatLng(-25.363882,131.044922)
@@ -29,30 +32,25 @@ function addMarker(lat, lng, title) {
   const marker = new google.maps.Marker({
     position: myLatlng,
     map: map,
-    title: "Test Bathroom"
+    title: 'Test Bathroom',
   });
 }
-
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
-    center: SAN_FRANCISCO,
-    restriction: {
-      latLngBounds: SAN_FRANCISCO_BOUNDS,
-      strictBounds: false,
-    },
-    styles: myStyles
+  loader.load().then(() => {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: SAN_FRANCISCO,
+      restriction: {
+        latLngBounds: SAN_FRANCISCO_BOUNDS,
+        strictBounds: false,
+      },
+      styles: myStyles,
+    });
   });
 
-  addMarker(37.7882589, -122.4104123, "Market/Powell");
+  addMarker(37.7882589, -122.4104123, 'Market/Powell');
 }
-
-
-
-
-
-
 
 // // The marker, positioned at Uluru
 // const marker = new google.maps.Marker({
