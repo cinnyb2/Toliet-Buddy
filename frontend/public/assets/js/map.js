221b863ee30b462578227e076cbc27f613d0ac8f
Andre Hu <andrebhu@gmail.com>
2022-03-26 18:21:03 -0400

    Remove POIs, addMarker function

--- frontend/public/assets/js/map.js
+++ frontend/public/assets/js/map.js
@@ -10,18 +10,51 @@ const SAN_FRANCISCO_BOUNDS = {
 };
 const SAN_FRANCISCO = { lat: 37.76, lng: -122.45};
 
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 13,
-      center: SAN_FRANCISCO,
-      restriction: {
-        latLngBounds: SAN_FRANCISCO_BOUNDS,
-        strictBounds: false,
-      }
-    });
+var myStyles =[
+  {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+            { visibility: "off" }
+      ]
   }
+];
 
-  // // The marker, positioned at Uluru
+
+
+function addMarker(lat, lng, title) {
+  // var myLatlng = new google.maps.LatLng(-25.363882,131.044922)
+  var myLatlng = new google.maps.LatLng(lat, lng);
+
+  const marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: "Test Bathroom"
+  });
+}
+
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: SAN_FRANCISCO,
+    restriction: {
+      latLngBounds: SAN_FRANCISCO_BOUNDS,
+      strictBounds: false,
+    },
+    styles: myStyles
+  });
+
+  addMarker(37.7882589, -122.4104123, "Market/Powell");
+}
+
+
+
+
+
+
+
+// // The marker, positioned at Uluru
 // const marker = new google.maps.Marker({
 //   position: uluru,
 //   map: map,
