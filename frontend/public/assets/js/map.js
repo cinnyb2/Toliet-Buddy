8c02aafa515d83c6d2605a3368f435770b7998d5
Andre Hu <andrebhu@gmail.com>
2022-03-26 18:07:37 -0400

    Add map boundaries

--- frontend/public/assets/js/map.js
+++ frontend/public/assets/js/map.js
@@ -1,15 +1,28 @@
 // Initialize and add the map
+
+
+let map;
+const SAN_FRANCISCO_BOUNDS = {
+  north: 37.84,
+  south: 37.71,
+  west: -122.52,
+  east: -122.35
+};
+const SAN_FRANCISCO = { lat: 37.76, lng: -122.45};
+
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+      zoom: 13,
+      center: SAN_FRANCISCO,
+      restriction: {
+        latLngBounds: SAN_FRANCISCO_BOUNDS,
+        strictBounds: false,
+      }
     });
   }
+
+  // // The marker, positioned at Uluru
+// const marker = new google.maps.Marker({
+//   position: uluru,
+//   map: map,
+// });
