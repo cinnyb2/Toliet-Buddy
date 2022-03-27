e183c24fe4dcb004df55f543b723a70cfbf99130
Melanie Ta <nngoc2701@gmail.com>
2022-03-27 01:51:53 -0700

    Add style to see all reviews

--- frontend/public/assets/js/review-form.js
+++ frontend/public/assets/js/review-form.js
@@ -46,16 +46,20 @@ function displayCards(collection) {
                 if (doc.exists) {
                     let CardTemplate = document.getElementById("CardTemplate");
                     let newcard = CardTemplate.cloneNode(true);
+                    newcard.id = "preview";
                     newcard.querySelector(".card-title").innerHTML =
                         doc.data().reviewTitle;
                     newcard.querySelector(".card-location").innerHTML =
-                        doc.data().location;
+                        "<b>Location:</b> " + doc.data().location;
                     newcard.querySelector(".card-rating").innerHTML =
-                        doc.data().rating;
+                        "<b>Ratings:</b>" + doc.data().rating;
                     newcard.querySelector(".card-content").innerHTML =
-                        doc.data().reviewContent;
+                        "<b>Content:</b> " + doc.data().reviewContent;
                     newcard.querySelector(".card-sentiment").innerHTML =
-                        doc.data().sentiment[1] + " " + doc.data().sentiment[0];
+                        "<b>Sentiment Analysis:</b> " +
+                        doc.data().sentiment[1] +
+                        " " +
+                        doc.data().sentiment[0];
                     document
                         .getElementById("review_container")
                         .appendChild(newcard);
